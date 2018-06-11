0b097a58bd532ab570065b4681e358cfcec2d129
sum01 <sum01@protonmail.com>
2018-06-11 02:20:31 -0400

    Add path to Qt5 in toolchain
    Specifically the path to the qt5base-mingw-w64 Qt5Config.cmake file

--- cmake/windows-toolchain.cmake
+++ cmake/windows-toolchain.cmake
@@ -3,6 +3,8 @@ set(CMAKE_SYSTEM_PROCESSOR "AMD64")
 
 # Supposedly this is 64-bit, and Ubuntu calls it this (I think?)
 set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++")
+# Specific to the location of 'qt5base-mingw-w64' Ubuntu ppa package
+set(Qt5_DIR "/usr/x86_64-w64-mingw32/lib/cmake/Qt5")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
